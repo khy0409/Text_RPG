9cc8e3211325dec4d4d8de108f0a3cee5238dcfc
khy0409 <124324468+khy0409@users.noreply.github.com>
2023-07-04 18:57:15 +0900

    Add files via upload
    Text_RPG(do not healer)

---
+++ Monster.java
@@ -0,0 +1,59 @@
+package TEXT_RPG;
+
+public class Monster extends Creature{
+	Monster() {
+		this.str = 10;
+		this.hp = 150;
+	}
+
+	void attackAll(Creature[] targetArr) {
+		for (int i = 0; i < targetArr.length; i++) {
+			attackType(targetArr[i]);
+		}
+	}
+
+	@Override
+	public void attack(Creature target) {
+		if (target.hp >= this.str) {
+			target.hp -= this.str;
+		} else {
+			target.hp = 0;
+		}
+
+		String playerName = target instanceof Player ? ((Player) target).name : "";
+		System.out.println("몬스터가 " + playerName + " 유저를 공격합니다");
+	}
+
+	@Override
+	public void attackType(Creature target) {
+		boolean typeHit = TypeCheck(this.type, target.type);
+
+		// typeHit일때는 str1.5배보다 hp가 많이 남았나 체크해야함
+		if (typeHit)
+			DecreaseHpTypeHit(target);
+		else
+			DecreaseHp(target);
+
+		String hitInfo = typeHit ? "[Type Hit!] " : "";
+		String playerName = target instanceof Player ? ((Player) target).name : "";
+		System.out.println(hitInfo + "몬스터가 " + playerName + " 유저를 공격합니다");
+	}
+
+	@Override
+	public String toString() {
+		return "Monster [str=" + str + ", hp=" + hp + ", type=" + type + "]";
+	}
+
+	@Override
+	public void heal(Creature target) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void healType(Creature target) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Thief.java
@@ -0,0 +1,33 @@
+package TEXT_RPG;
+
+public class Thief extends Player2ndLevel {
+	public Thief(String name) {
+		super(name);
+		this.skillMp = 20;
+	}
+
+	@Override
+	void skill(Creature target) {
+		boolean typeHit = TypeCheck(this.type, target.type);
+		this.mp -= skillMp;
+		
+		if(typeHit)
+			decreaseHpTypeHitBySkill(target, 2);
+		else
+			decreaseHpBySkill(target, 2);
+		
+		String hitInfo = typeHit ? "[Type Hit!] " : "";
+		System.out.println(hitInfo + "도적 " + this.name + " 유저가 몬스터를 기습했습니다");
+
+	}
+
+	@Override
+	public String toString() {
+		return "Thief [skillMp=" + skillMp + ", name=" + name + ", mp=" + mp + ", str=" + str + ", hp=" + hp + ", type="
+				+ type + "]";
+	}
+
+	@Override
+	void skill(Creature[] target) {
+	}
+}
